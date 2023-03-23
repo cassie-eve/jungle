8d89a0c99f3996e0e4d61ea6f026dcf17516d2f0
Cassandra Keddis <cassandra@thinkific.com>
2023-03-22 20:24:14 -0700

    email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 1 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
   has_secure_password
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
+    it "email must be unique" do
+      @user = User.new(first_name: "Cassie", last_name: "Keddis", email: "cass@gmail.com", password: '123Password', password_confirmation: '123Password')
+      @user.save
+
+      @user2 = User.new(first_name: "Cassie", last_name: "Keddis", email: "cass@gmail.com", password: '123Password', password_confirmation: '123Password')
+      @user2.save
+      
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
 
   end
 end
